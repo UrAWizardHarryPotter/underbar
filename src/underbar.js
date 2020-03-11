666a95a3dfb4adf7f07e4eaff076db484bdd1190
Zachary Schwartzman <zach.schwartzman@gmail.com>
2020-03-11 01:00:15 -0700

    Define ._extend and ._defaults in Objects

--- src/underbar.js
+++ src/underbar.js
@@ -247,11 +247,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(newProp) { // -> for each called arguments, invoke function with newProp
+      for (let key in newProp) { // -> run for loop over newProp
+        obj[key] = newProp[key]; // -> assign obj[key] to newProp[key]
+      }
+    });
+    return obj; // -> return obj
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(newProp) { // -> for each called arguments, invoke function with newProp
+      for (let key in newProp) { // -> run for loop over newProp
+        if (obj[key] === undefined) { // -> if obj[key] doesn't exist (undefined)
+          obj[key] = newProp[key]; // -> assign obj[key] to newProp[key]
+        }
+      }
+    });
+    return obj; // -> return obj
   };
 
 
